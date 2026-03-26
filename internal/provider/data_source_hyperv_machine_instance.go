Bafbi/terraform-provider-hyperv

commit 00909b3ecdc04c5978a37e543ae4e697a33fd313

fix path normalization across Hyper-V path fields
Normalize and compare paths consistently across VM/VHD resources and data sources so Windows/Unix separator and drive-prefix differences do not cause drift. This also makes the VHD acceptance check robust to equivalent Windows-form paths returned by Hyper-V.

internal/provider/data_source_hyperv_machine_instance.go
@@ -25,34 +25,13 @@ func dataSourceHyperVMachineInstance() *schema.Resource {
 			},
 
 			"path": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-				ForceNew: true,
-				DiffSuppressFunc: func(k, oldValue, newValue string, d *schema.ResourceData) bool {
-					if newValue == "" {
-						return true
-					}
-
-					// When specifying path on new-vm it will auto append machine name on the end
-					name := d.Get("name").(string)
-					computedPath := newValue
-					if !strings.HasSuffix(computedPath, "\\") {
-						computedPath += "\\"
-					}
-					computedPath += name
-
-					if strings.EqualFold(computedPath, oldValue) {
-						return true
-					}
-
-					if strings.EqualFold(oldValue, newValue) {
-						return true
-					}
-
-					return false
-				},
-				Description: "The path of the virtual machine.",
+				Type:             schema.TypeString,
+				Optional:         true,
+				Computed:         true,
+				ForceNew:         true,
+				StateFunc:        PathStateFunc,
+				DiffSuppressFunc: PathDiffSuppressWithMachineName,
+				Description:      "The path of the virtual machine.",
 			},
 
 			"generation": {
@@ -185,6 +164,7 @@ func dataSourceHyperVMachineInstance() *schema.Resource {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Default:     `C:\ProgramData\Microsoft\Windows\Hyper-V`,
+				StateFunc:   PathStateFunc,
 				Description: "Specifies the folder in which the Smart Paging file is to be stored.",
 			},
 
@@ -192,6 +172,7 @@ func dataSourceHyperVMachineInstance() *schema.Resource {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Default:     `C:\ProgramData\Microsoft\Windows\Hyper-V`,
+				StateFunc:   PathStateFunc,
 				Description: "Specifies the folder in which the virtual machine is to store its snapshot files.",
 			},
 
@@ -614,10 +595,12 @@ func dataSourceHyperVMachineInstance() *schema.Resource {
 							Description: "Specifies the number of the location on the controller at which the DVD drive is to be added. ",
 						},
 						"path": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Default:     "",
-							Description: "Specifies the full path to the virtual hard disk file or physical hard disk volume for the added DVD drive.",
+							Type:             schema.TypeString,
+							Optional:         true,
+							Default:          "",
+							DiffSuppressFunc: PathDiffSuppress,
+							StateFunc:        PathStateFunc,
+							Description:      "Specifies the full path to the virtual hard disk file or physical hard disk volume for the added DVD drive.",
 						},
 						"resource_pool_name": {
 							Type:        schema.TypeString,
@@ -656,6 +639,7 @@ func dataSourceHyperVMachineInstance() *schema.Resource {
 							Optional:         true,
 							Default:          "",
 							DiffSuppressFunc: api.DiffSuppressVmHardDiskPath,
+							StateFunc:        PathStateFunc,
 							Description:      "Specifies the full path of the hard disk drive file to be added.",
 						},
 						"disk_number": {
@@ -779,9 +763,10 @@ func dataSourceHyperVMachineInstance() *schema.Resource {
 										Description: "Specifies the mac address of ethernet adapter.",
 									},
 									"path": {
-										Type:     schema.TypeString,
-										Optional: true,
-										Default:  "",
+										Type:      schema.TypeString,
+										Optional:  true,
+										Default:   "",
+										StateFunc: PathStateFunc,
 										DiffSuppressFunc: func(k, oldValue, newValue string, d *schema.ResourceData) bool {
 											bootTypeKey := strings.Replace(k, "path", "boot_type", 1)
 											bootType := d.Get(bootTypeKey).(string)
@@ -789,27 +774,7 @@ func dataSourceHyperVMachineInstance() *schema.Resource {
 												return true
 											}
 
-											if newValue == "" {
-												return true
-											}
-
-											// When specifying path on new-vm it will auto append machine name on the end
-											name := d.Get("name").(string)
-											computedPath := newValue
-											if !strings.HasSuffix(computedPath, "\\") {
-												computedPath += "\\"
-											}
-											computedPath += name
-
-											if strings.EqualFold(computedPath, oldValue) {
-												return true
-											}
-
-											if strings.EqualFold(oldValue, newValue) {
-												return true
-											}
-
-											return false
+											return PathDiffSuppressWithMachineName(k, oldValue, newValue, d)
 										},
 										Description: "Specifies the file path of hard disk drive or dvd drive.",
 									},

internal/provider/resource_hyperv_machine_instance.go
@@ -47,39 +47,13 @@ func resourceHyperVMachineInstance() *schema.Resource {
 			},
 
 			"path": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
-				ForceNew: true,
-				DiffSuppressFunc: func(k, oldValue, newValue string, d *schema.ResourceData) bool {
-					if newValue == "" {
-						return true
-					}
-
-					// Normalize path separators for comparison (Windows accepts both / and \)
-					oldNormalized := api.NormalizePath(oldValue)
-					newNormalized := api.NormalizePath(newValue)
-
-					// When specifying path on new-vm it will auto append machine name on the end
-					name := d.Get("name").(string)
-					computedPath := newNormalized
-					if !strings.HasSuffix(computedPath, "/") {
-						computedPath += "/"
-					}
-					computedPath += name
-
-					if strings.EqualFold(computedPath, oldNormalized) {
-						return true
-					}
-
-					if strings.EqualFold(oldNormalized, newNormalized) {
-						return true
-					}
-
-					return false
-				},
-				StateFunc:   PathStateFunc,
-				Description: "The path of the virtual machine.",
+				Type:             schema.TypeString,
+				Optional:         true,
+				Computed:         true,
+				ForceNew:         true,
+				DiffSuppressFunc: PathDiffSuppressWithMachineName,
+				StateFunc:        PathStateFunc,
+				Description:      "The path of the virtual machine.",
 			},
 
 			"generation": {
@@ -213,6 +187,7 @@ func resourceHyperVMachineInstance() *schema.Resource {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Default:     `C:\ProgramData\Microsoft\Windows\Hyper-V`,
+				StateFunc:   PathStateFunc,
 				Description: "Specifies the folder in which the Smart Paging file is to be stored.",
 			},
 
@@ -220,6 +195,7 @@ func resourceHyperVMachineInstance() *schema.Resource {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Default:     `C:\ProgramData\Microsoft\Windows\Hyper-V`,
+				StateFunc:   PathStateFunc,
 				Description: "Specifies the folder in which the virtual machine is to store its snapshot files.",
 			},
 
@@ -645,17 +621,12 @@ func resourceHyperVMachineInstance() *schema.Resource {
 							Description: "Specifies the number of the location on the controller at which the DVD drive is to be added.",
 						},
 						"path": {
-							Type:     schema.TypeString,
-							Optional: true,
-							Default:  "",
-							DiffSuppressFunc: func(k, oldValue, newValue string, d *schema.ResourceData) bool {
-								// Normalize path separators for comparison (Windows accepts both / and \)
-								oldNormalized := api.NormalizePath(oldValue)
-								newNormalized := api.NormalizePath(newValue)
-								return strings.EqualFold(oldNormalized, newNormalized)
-							},
-							StateFunc:   PathStateFunc,
-							Description: "Specifies the full path to the virtual hard disk file or physical hard disk volume for the added DVD drive.",
+							Type:             schema.TypeString,
+							Optional:         true,
+							Default:          "",
+							DiffSuppressFunc: PathDiffSuppress,
+							StateFunc:        PathStateFunc,
+							Description:      "Specifies the full path to the virtual hard disk file or physical hard disk volume for the added DVD drive.",
 						},
 						"resource_pool_name": {
 							Type:     schema.TypeString,
@@ -846,31 +817,7 @@ func resourceHyperVMachineInstance() *schema.Resource {
 												return true
 											}
 
-											if newValue == "" {
-												return true
-											}
-
-											// Normalize path separators for comparison (Windows accepts both / and \)
-											oldNormalized := api.NormalizePath(oldValue)
-											newNormalized := api.NormalizePath(newValue)
-
-											// When specifying path on new-vm it will auto append machine name on the end
-											name := d.Get("name").(string)
-											computedPath := newNormalized
-											if !strings.HasSuffix(computedPath, "/") {
-												computedPath += "/"
-											}
-											computedPath += name
-
-											if strings.EqualFold(computedPath, oldNormalized) {
-												return true
-											}
-
-											if strings.EqualFold(oldNormalized, newNormalized) {
-												return true
-											}
-
-											return false
+											return PathDiffSuppressWithMachineName(k, oldValue, newValue, d)
 										},
 										StateFunc:   PathStateFunc,
 										Description: "Specifies the file path of hard disk drive or dvd drive.",

api/vm_hard_disk_drive.go
@@ -131,10 +131,10 @@ func DiffSuppressVmHardDiskPath(key, old, new string, d *schema.ResourceData) bo
 	}
 
 	// Normalize path separators for comparison (Windows accepts both / and \)
-	oldNormalized := strings.ReplaceAll(old, "\\", "/")
-	newNormalized := strings.ReplaceAll(new, "\\", "/")
+	oldNormalized := NormalizePath(old)
+	newNormalized := NormalizePath(new)
 
-	if newNormalized == oldNormalized {
+	if strings.EqualFold(newNormalized, oldNormalized) {
 		return true
 	}
 

internal/provider/data_source_hyperv_vhd.go
@@ -20,11 +20,13 @@ func dataSourceHyperVVhd() *schema.Resource {
 			"path": {
 				Type:        schema.TypeString,
 				Required:    true,
+				StateFunc:   PathStateFunc,
 				Description: "Path to the existing virtual hard disk file(s) that is being created or being copied to. If a filename or relative path is specified, the virtual hard disk path is calculated relative to the current working directory. Depending on the source selected, the path will be used to determine where to copy source vhd/vhdx/vhds file to.",
 			},
 			"source": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:      schema.TypeString,
+				Optional:  true,
+				StateFunc: PathStateFunc,
 				ConflictsWith: []string{
 					"source_vm",
 					"parent_path",
@@ -64,8 +66,9 @@ func dataSourceHyperVVhd() *schema.Resource {
 				Description: "VHD type to use. Valid values to use are `Unknown`, `Fixed`, `Dynamic`, `Differencing`.",
 			},
 			"parent_path": {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:      schema.TypeString,
+				Optional:  true,
+				StateFunc: PathStateFunc,
 				ConflictsWith: []string{
 					"source",
 					"source_vm",
@@ -150,7 +153,7 @@ func datasourceHyperVVhdRead(ctx context.Context, d *schema.ResourceData, meta i
 		return diag.FromErr(err)
 	}
 
-	if vhd.Path != "" {
+	if vhd.Path == "" {
 		log.Printf("[INFO][hyperv][read] unable to retrieved vhd: %+v", path)
 		if err := d.Set("exists", false); err != nil {
 			return diag.FromErr(err)

internal/provider/data_source_hyperv_vhd_test.go
@@ -6,9 +6,12 @@ package provider
 import (
 	"fmt"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/taliesins/terraform-provider-hyperv/api"
 )
 
 func TestHyperVDataSourceVhd(t *testing.T) {
@@ -28,12 +31,45 @@ func TestHyperVDataSourceVhd(t *testing.T) {
 			{
 				Config: testHyperDataSourceVVhdConfig(path),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"data.hyperv_vhd.this", "path", path),
+					testCheckPathEquivalent("data.hyperv_vhd.this", "path", path),
 				),
 			},
 		},
 	})
+}
+
+func testCheckPathEquivalent(resourceName, attributeName, expectedPath string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		resourceState, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("resource not found in state: %s", resourceName)
+		}
+
+		actualPath, ok := resourceState.Primary.Attributes[attributeName]
+		if !ok {
+			return fmt.Errorf("attribute not found in state: %s.%s", resourceName, attributeName)
+		}
+
+		expectedNormalized := strings.ToLower(api.NormalizePath(expectedPath))
+		actualNormalized := strings.ToLower(api.NormalizePath(actualPath))
+
+		if expectedNormalized == actualNormalized {
+			return nil
+		}
+
+		if len(actualNormalized) > 2 && actualNormalized[1] == ':' {
+			withoutDrive := actualNormalized[2:]
+			if !strings.HasPrefix(withoutDrive, "/") {
+				withoutDrive = "/" + withoutDrive
+			}
+
+			if withoutDrive == expectedNormalized {
+				return nil
+			}
+		}
+
+		return fmt.Errorf("attribute '%s' expected %q or Windows-drive equivalent, got %q", attributeName, expectedNormalized, actualNormalized)
+	}
 }
 
 func testHyperDataSourceVVhdConfig(path string) string {

internal/provider/path_helper.go
@@ -11,11 +11,45 @@ func PathStateFunc(val interface{}) string {
 	return api.NormalizePath(val.(string))
 }
 
+func normalizeComparablePath(path string) string {
+	normalized := api.NormalizePath(path)
+	if len(normalized) > 2 && normalized[1] == ':' {
+		normalized = normalized[2:]
+		if !strings.HasPrefix(normalized, "/") {
+			normalized = "/" + normalized
+		}
+	}
+
+	return normalized
+}
+
 func PathDiffSuppress(k, old, new string, d *schema.ResourceData) bool {
 	if new == "" {
 		return true
 	}
-	oldNormalized := api.NormalizePath(old)
-	newNormalized := api.NormalizePath(new)
+	oldNormalized := normalizeComparablePath(old)
+	newNormalized := normalizeComparablePath(new)
+	return strings.EqualFold(oldNormalized, newNormalized)
+}
+
+func PathDiffSuppressWithMachineName(k, old, new string, d *schema.ResourceData) bool {
+	if new == "" {
+		return true
+	}
+
+	oldNormalized := normalizeComparablePath(old)
+	newNormalized := normalizeComparablePath(new)
+
+	name := d.Get("name").(string)
+	computedPath := newNormalized
+	if !strings.HasSuffix(computedPath, "/") {
+		computedPath += "/"
+	}
+	computedPath += name
+
+	if strings.EqualFold(computedPath, oldNormalized) {
+		return true
+	}
+
 	return strings.EqualFold(oldNormalized, newNormalized)
 }

internal/provider/resource_hyperv_vhd.go
@@ -41,9 +41,8 @@ func resourceHyperVVhd() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 				DiffSuppressFunc: func(k, oldValue, newValue string, d *schema.ResourceData) bool {
-					// Normalize path separators for comparison (Windows accepts both / and \)
-					oldNormalized := api.NormalizePath(oldValue)
-					newNormalized := api.NormalizePath(newValue)
+					oldNormalized := normalizeComparablePath(oldValue)
+					newNormalized := normalizeComparablePath(newValue)
 
 					extension := path.Ext(newNormalized)
 					computedPath := strings.TrimSuffix(newNormalized, extension)

commit a66b584d8826b4c05e4e2cf597cf906db12b3bec

fix(provider): add missing DiffSuppressFunc for path fields
Add PathDiffSuppress to smart_paging_file_path and snapshot_file_location
fields in both resource and data source. These fields had StateFunc but were
missing the corresponding DiffSuppressFunc, causing false positive diffs when
Hyper-V returns paths with backslashes and Terraform config uses forward slashes.

This was a regression introduced in commit 00909b3 where StateFunc was added
but DiffSuppressFunc was forgotten, while other path fields were properly
updated with both.

internal/provider/data_source_hyperv_machine_instance.go
@@ -162,19 +162,21 @@ func dataSourceHyperVMachineInstance() *schema.Resource {
 			},
 
 			"smart_paging_file_path": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     `C:\ProgramData\Microsoft\Windows\Hyper-V`,
-				StateFunc:   PathStateFunc,
-				Description: "Specifies the folder in which the Smart Paging file is to be stored.",
+				Type:             schema.TypeString,
+				Optional:         true,
+				Default:          `C:\ProgramData\Microsoft\Windows\Hyper-V`,
+				DiffSuppressFunc: PathDiffSuppress,
+				StateFunc:        PathStateFunc,
+				Description:      "Specifies the folder in which the Smart Paging file is to be stored.",
 			},
 
 			"snapshot_file_location": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     `C:\ProgramData\Microsoft\Windows\Hyper-V`,
-				StateFunc:   PathStateFunc,
-				Description: "Specifies the folder in which the virtual machine is to store its snapshot files.",
+				Type:             schema.TypeString,
+				Optional:         true,
+				Default:          `C:\ProgramData\Microsoft\Windows\Hyper-V`,
+				DiffSuppressFunc: PathDiffSuppress,
+				StateFunc:        PathStateFunc,
+				Description:      "Specifies the folder in which the virtual machine is to store its snapshot files.",
 			},
 
 			"static_memory": {

internal/provider/resource_hyperv_machine_instance.go
@@ -185,19 +185,21 @@ func resourceHyperVMachineInstance() *schema.Resource {
 			},
 
 			"smart_paging_file_path": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     `C:\ProgramData\Microsoft\Windows\Hyper-V`,
-				StateFunc:   PathStateFunc,
-				Description: "Specifies the folder in which the Smart Paging file is to be stored.",
+				Type:             schema.TypeString,
+				Optional:         true,
+				Default:          `C:\ProgramData\Microsoft\Windows\Hyper-V`,
+				DiffSuppressFunc: PathDiffSuppress,
+				StateFunc:        PathStateFunc,
+				Description:      "Specifies the folder in which the Smart Paging file is to be stored.",
 			},
 
 			"snapshot_file_location": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     `C:\ProgramData\Microsoft\Windows\Hyper-V`,
-				StateFunc:   PathStateFunc,
-				Description: "Specifies the folder in which the virtual machine is to store its snapshot files.",
+				Type:             schema.TypeString,
+				Optional:         true,
+				Default:          `C:\ProgramData\Microsoft\Windows\Hyper-V`,
+				DiffSuppressFunc: PathDiffSuppress,
+				StateFunc:        PathStateFunc,
+				Description:      "Specifies the folder in which the virtual machine is to store its snapshot files.",
 			},
 
 			"static_memory": {